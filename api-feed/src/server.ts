d874bbb85bea6fc908a7a6a091ecfee541e220db
sebastien6 <sebastien6@outlook.com>
2020-04-21 18:15:06 -0400

    test npm cors

--- api-feed/src/server.ts
+++ api-feed/src/server.ts
@@ -1,4 +1,5 @@
 import express from 'express';
+import cors from 'cors';
 import { sequelize } from './sequelize';
 
 import { IndexRouter } from './controllers/v0/index.router';
@@ -17,11 +18,7 @@ import { V0MODELS } from './controllers/v0/model.index';
   app.use(bodyParser.json());
 
   //CORS Should be restricted
-  app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    // res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
-    next();
-  });
+  app.use(cors())
 
   app.use('/api/v0/', IndexRouter)
 
